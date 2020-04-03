77f7c82adf6dffa617581d95cf752641dd927beb
Shahed Mohammad Hridoy <35490924+ShahedMohammadHridoy@users.noreply.github.com>
2020-04-03 14:25:55 +0600

    index page

---
+++ login_page/login1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="login-box">
+            <h1>Login</h1>
+            <div class="textbox">
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Username">
+            </div>
+            <div class="textbox">
+                <i class="fas fa-lock"></i>
+                <input type="password" placeholder="Password">
+            </div>
+            <input type="submit" value="Sign In" class="btn">
+        </div>
+    </body>
+</html>
